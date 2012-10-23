d30fdd31a3885f65718e4796a56b4ba422ad70f8
Christian Schwarz <christianschw4rz@gmail.com>
2012-10-24 01:50:00 +0200

    - added stub for exponential smoothing

---
+++ pycast/methods/exponentialsmoothing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#Copyright (c) 2012 Christian Schwarz
+#
+#Permission is hereby granted, free of charge, to any person obtaining
+#a copy of this software and associated documentation files (the
+#"Software"), to deal in the Software without restriction, including
+#without limitation the rights to use, copy, modify, merge, publish,
+#distribute, sublicense, and/or sell copies of the Software, and to
+#permit persons to whom the Software is furnished to do so, subject to
+#the following conditions:
+#
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+#WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from basemethod import BaseMethod
+from pycast.common.timeseries import TimeSeries
+
+class ExponentialSmoothing(BaseMethod):
+    """Implements an exponential smoothing algorithm.
+
+    Explanation: http://en.wikipedia.org/wiki/Exponential_smoothing
+    """
+
+    def __init__(self):
+        """Initializes the ExponentialSmoothing.
+        """
+        super(SimpleMovingAverage, self).__init__([], True, True)
+
+    def execute(self, timeSeries):
+        """Creates a new TimeSeries containing the exponentially smoothed values.
+
+        @return TimeSeries object containing the smooth moving average.
+        
+        @todo This implementation aims to support independent for loop execution.
+        """
+        res = TimeSeries()
+
+        return res
