0f4f5491e858971c750135b0e5749f472cadba77
Christian Schwarz <christianschw4rz@gmail.com>
2012-10-24 09:07:08 +0200

    - added gnuplot time series export - unified docstrings

--- pycast/methods/exponentialsmoothing.py
+++ pycast/methods/exponentialsmoothing.py
@@ -73,7 +73,7 @@ class ExponentialSmoothing(BaseMethod):
             startTime          = res[-1][0]
             normalizedTimeDiff = startTime - res[-2][0]
 
-            for idx in xrange(self._parameters["valuesToForecast"]):
+            for idx in xrange(valuesToForecast):
                 startTime += normalizedTimeDiff
 
                 s = (alpha * res[-1][1]) + ((1 - alpha) * s)
@@ -89,7 +89,7 @@ class HoltMethod(BaseMethod):
     """
 
     def __init__(self, smoothingFactor=0.1, trendSmoothingFactor=0.5, valuesToForecast=1):
-        """Initializes the ExponentialSmoothing.
+        """Initializes the HoltMethod.
 
         @param smoothingFactor Defines the alpha for the HoltMethod.
         @param trendSmoothingFactor Defines the beta for the HoltMethod.
@@ -123,25 +123,81 @@ class HoltMethod(BaseMethod):
         valuesToForecast = self._parameters["valuesToForecast"]
 
         ## smooth the existing TimeSeries data
-#       for idx in xrange(len(timeSeries)):
-#           if 0 == idx:
-#               s = timeSeries[idx][1]
-#               continue
-#
-#           s = (alpha * timeSeries[idx-1][1]) + ((1 - alpha) * s)
-#
-#           res.add_entry(timeSeries[idx][0], s)
+       for idx in xrange(len(timeSeries)):
+           ## initialization for s_1 and b_1
+           if 0 == idx:
+               sOld = timeSeries[idx][1]
+               b = timeSeries[idx+1][1]-timeSeries[idx][1]
+               continue
+
+            sNew = (alpha * timeSeries[idx-1][1]) + ((1 - alpha) * (sOld + b))
+            b    = (beta * (sNew - sOld)) + ((1 - beta) * b)
+            sOld = sNew
+
+            res.add_entry(timeSeries[idx][0], sOld)
 
         ## forecast additional values if requested
-#        if valuesToForecast > 0:
-#            startTime          = res[-1][0]
-#            normalizedTimeDiff = startTime - res[-2][0]
-#
-#            for idx in xrange(self._parameters["valuesToForecast"]):
-#                startTime += normalizedTimeDiff
-#
-#                s = (alpha * res[-1][1]) + ((1 - alpha) * s)
-#                res.add_entry(startTime, s)
+        if valuesToForecast > 0:
+            startTime          = res[-1][0]
+            normalizedTimeDiff = startTime - res[-2][0]
+
+            for idx in xrange(valuesToForecast):
+                startTime += normalizedTimeDiff
+
+                s = sOld + idx * b
+                res.add_entry(startTime, s)
+
+        ## return the resulting TimeSeries :)
+        return res
+    
+    ## TODO:A second method, referred to as either Brown's linear exponential smoothing (LES) or Brown's double exponential smoothing works as follows.[9]
+
+class HoltWintersMethod(BaseMethod):
+    """Implements the Holt-Winters algorithm.
+
+    Explanation: http://en.wikipedia.org/wiki/Exponential_smoothing#Triple_exponential_smoothing
+
+    @todo NotImplementedYet
+    """
+
+    def __init__(self, smoothingFactor=0.1, trendSmoothingFactor=0.5, seasonLength=42, valuesToForecast=1):
+        """Initializes the HoltWintersMethod.
+
+        @param smoothingFactor Defines the alpha for the HoltMethod.
+        @param trendSmoothingFactor Defines the beta for the HoltMethod.
+        @param seasonLength The expected length for the seasons. Please use a good estimate here!
+        @param valuesToForecast Defines the number of forecasted values that will
+               be part of the result.
+        """
+        super(HoltWintersMethod, self).__init__(["smoothingFactor",
+                                          "trendSmoothingFactor", 
+                                          "valuesToForecast",
+                                          "seasonLength"],
+                                          True, True)
+
+        self.add_parameter("smoothingFactor",      smoothingFactor)
+        self.add_parameter("trendSmoothingFactor", trendSmoothingFactor)
+        seld.add_parameter("seasonLength",         seasonLength)
+        self.add_parameter("valuesToForecast",     valuesToForecast)
+
+    def execute(self, timeSeries):
+        """Creates a new TimeSeries containing the smoothed values.
+
+        @return TimeSeries object containing the exponentially smoothed TimeSeries,
+                including the forecasted values.
+        
+        @todo Double check if it is correct not to add the first original value to the result.
+        @todo Currently the first normalized value is simply chosen as the starting point.
+        """
+        ## initialize the result TimeSeries
+        res = TimeSeries()
+
+        ## extract the required parameters, performance improvement
+        alpha            = self._parameters["smoothingFactor"]
+        beta             = self._parameters["trendSmoothingFactor"]
+        valuesToForecast = self._parameters["valuesToForecast"]
+
+        ## @todo THIS IS NOT IMPLEMENTED YET
 
         ## return the resulting TimeSeries :)
         return res
