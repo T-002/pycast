f2ee93f0c19d024566a51b4954a5d6a908da10a7
Christian Schwarz <christianschw4rz@gmail.com>
2012-10-24 05:44:21 +0200

    - minor typo fixes - added stub for Holt's method

--- pycast/methods/exponentialsmoothing.py
+++ pycast/methods/exponentialsmoothing.py
@@ -33,15 +33,20 @@ class ExponentialSmoothing(BaseMethod):
 
     def __init__(self, smoothingFactor=0.1, valuesToForecast=1):
         """Initializes the ExponentialSmoothing.
+
+        @param smoothingFactor Defines the alpha for the ExponentialSmoothing.
+        @param valuesToForecast Defines the number of forecasted values that will
+               be part of the result.
         """
         super(ExponentialSmoothing, self).__init__(["smoothingFactor", "valuesToForecast"], True, True)
         self.add_parameter("smoothingFactor", smoothingFactor)
         self.add_parameter("valuesToForecast", valuesToForecast)
 
     def execute(self, timeSeries):
-        """Creates a new TimeSeries containing the exponentially smoothed values.
+        """Creates a new TimeSeries containing the smoothed values.
 
-        @return TimeSeries object containing the smooth moving average.
+        @return TimeSeries object containing the exponentially smoothed TimeSeries,
+                including the forecasted values.
         
         @todo Double check if it is correct not to add the first original value to the result.
         @todo Currently the first normalized value is simply chosen as the starting point.
@@ -73,6 +78,70 @@ class ExponentialSmoothing(BaseMethod):
 
                 s = (alpha * res[-1][1]) + ((1 - alpha) * s)
                 res.add_entry(startTime, s)
+
+        ## return the resulting TimeSeries :)
+        return res
+
+class HoltMethod(BaseMethod):
+    """Implements the Holt algorithm.
+
+    Explanation: http://en.wikipedia.org/wiki/Exponential_smoothing#Double_exponential_smoothing
+    """
+
+    def __init__(self, smoothingFactor=0.1, trendSmoothingFactor=0.5, valuesToForecast=1):
+        """Initializes the ExponentialSmoothing.
+
+        @param smoothingFactor Defines the alpha for the HoltMethod.
+        @param trendSmoothingFactor Defines the beta for the HoltMethod.
+        @param valuesToForecast Defines the number of forecasted values that will
+               be part of the result.
+        """
+        super(HoltMethod, self).__init__(["smoothingFactor",
+                                          "trendSmoothingFactor", 
+                                          "valuesToForecast"],
+                                          True, True)
+
+        self.add_parameter("smoothingFactor",      smoothingFactor)
+        self.add_parameter("trendSmoothingFactor", trendSmoothingFactor)
+        self.add_parameter("valuesToForecast",     valuesToForecast)
+
+    def execute(self, timeSeries):
+        """Creates a new TimeSeries containing the smoothed values.
+
+        @return TimeSeries object containing the exponentially smoothed TimeSeries,
+                including the forecasted values.
+        
+        @todo Double check if it is correct not to add the first original value to the result.
+        @todo Currently the first normalized value is simply chosen as the starting point.
+        """
+        ## initialize the result TimeSeries
+        res = TimeSeries()
+
+        ## extract the required parameters, performance improvement
+        alpha            = self._parameters["smoothingFactor"]
+        beta             = self._parameters["trendSmoothingFactor"]
+        valuesToForecast = self._parameters["valuesToForecast"]
+
+        ## smooth the existing TimeSeries data
+#       for idx in xrange(len(timeSeries)):
+#           if 0 == idx:
+#               s = timeSeries[idx][1]
+#               continue
+#
+#           s = (alpha * timeSeries[idx-1][1]) + ((1 - alpha) * s)
+#
+#           res.add_entry(timeSeries[idx][0], s)
+
+        ## forecast additional values if requested
+#        if valuesToForecast > 0:
+#            startTime          = res[-1][0]
+#            normalizedTimeDiff = startTime - res[-2][0]
+#
+#            for idx in xrange(self._parameters["valuesToForecast"]):
+#                startTime += normalizedTimeDiff
+#
+#                s = (alpha * res[-1][1]) + ((1 - alpha) * s)
+#                res.add_entry(startTime, s)
 
         ## return the resulting TimeSeries :)
         return res
