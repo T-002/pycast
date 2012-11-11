3f90c47c394930dc16eb553003c5fbe094c083da
Christian Schwarz <christianschw4rz@gmail.com>
2012-11-11 14:06:23 +0100

    - modified imports to match with wiki documentation

--- pycast/errors/meansquarederror.py
+++ pycast/errors/meansquarederror.py
@@ -22,7 +22,7 @@
 #OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 #WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from baseerrormeasure import BaseErrorMeasure
+from pycast.errors import BaseErrorMeasure
 
 class MeanSquaredError(BaseErrorMeasure):
     """Implements the mean squared error measure.
