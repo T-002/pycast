9c775c25ba99095632b8daf224d6c38d8f36ae1f
Christian Schwarz <christianschw4rz@gmail.com>
2012-12-14 14:30:55 +0100

    fixed decorator and test

--- pycast/common/decorators.py
+++ pycast/common/decorators.py
@@ -1,17 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#Copyright (c) 2012 Christian Schwarz
+#
+#Permission is hereby granted, free of charge, to any person obtaining
+#a copy of this software and associated documentation files (the
+#"Software"), to deal in the Software without restriction, including
+#without limitation the rights to use, copy, modify, merge, publish,
+#distribute, sublicense, and/or sell copies of the Software, and to
+#permit persons to whom the Software is furnished to do so, subject to
+#the following conditions:
+#
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+#WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from pycastobject import PyCastObject
 
 def optimized(fn):
-	def _optimized(self, *args, **kwargs):
-		if self.optimization_enabled:
-			class_name = self.__class__.__name__
-			module = self.__module__.replace('pycast', 'pycastC')
-			try:
-				imported = __import__(module+"."+class_name, globals(), locals(), [fn.__name__])
-				function = getattr(imported, fn.__name__)
-				function(self, *args, **kwargs)
-			except ImportError:
-				print "[WARNING] Could not enable optimization for %s, %s" % (fn.__name__, self)
-				fn(self, *args, **kwargs)
-		else:
-			fn(self, *args, **kwargs)
-	return _optimized
+    def _optimized(self, *args, **kwargs):
+        if self.optimization_enabled:
+            class_name = self.__class__.__name__
+            module = self.__module__.replace('pycast', 'pycastC')
+            try:
+                imported = __import__(module+"."+class_name, globals(), locals(), [fn.__name__])
+                function = getattr(imported, fn.__name__)
+                return function(self, *args, **kwargs)
+            except ImportError:
+                print "[WARNING] Could not enable optimization for %s, %s" % (fn.__name__, self)
+                return fn(self, *args, **kwargs)
+        else:
+            return fn(self, *args, **kwargs)
+    return _optimized
--- pycast/tests/decoratorstest.py
+++ pycast/tests/decoratorstest.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#Copyright (c) 2012 Christian Schwarz
+#
+#Permission is hereby granted, free of charge, to any person obtaining
+#a copy of this software and associated documentation files (the
+#"Software"), to deal in the Software without restriction, including
+#without limitation the rights to use, copy, modify, merge, publish,
+#distribute, sublicense, and/or sell copies of the Software, and to
+#permit persons to whom the Software is furnished to do so, subject to
+#the following conditions:
+#
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+#WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import unittest
 from mock import Mock
 
@@ -7,55 +31,55 @@ from pycast.errors.baseerrormeasure import BaseErrorMeasure
 import pycastC.errors.baseerrormeasure.BaseErrorMeasure as cerror
 
 class OptimizedDecoratorTest(unittest.TestCase):
-	def test_optimization_enabled(self):
-		error = BaseErrorMeasure()
-		error._enable_instance_optimization()
+    def test_optimization_enabled(self):
+        error = BaseErrorMeasure()
+        error._enable_instance_optimization()
 
-		cerror.initialize = Mock()
-		
-		error.initialize(None, None) #parameters irrelevant for this test
-		assert cerror.initialize.called, "If optimization is enabled the c method should be called"
+        cerror.initialize = Mock()
+        
+        error.initialize(None, None) #parameters irrelevant for this test
+        assert cerror.initialize.called, "If optimization is enabled the c method should be called"
 
-	def test_optimization_disabled(self):
-		error = BaseErrorMeasure()
-		error._disable_instance_optimization
+    def test_optimization_disabled(self):
+        error = BaseErrorMeasure()
+        error._disable_instance_optimization
 
-		cerror.initialize = Mock()
-		
-		try:
-			error.initialize(None, None) #parameters irrelevant for this test
-		except:
-			pass
-		assert not cerror.initialize.called, "If optimization is disabled the c method should not be called"
+        cerror.initialize = Mock()
+        
+        try:
+            error.initialize(None, None) #parameters irrelevant for this test
+        except:
+            pass
+        assert not cerror.initialize.called, "If optimization is disabled the c method should not be called"
 
-	def test_function_call_is_transparent(self):
-		"""
-		With and without optimization the method
-		should be called with the same parameters.
-		"""
-		error = BaseErrorMeasure()
-		error.local_error = Mock()
-		ts = TimeSeries.from_twodim_list([[1,1]])
+    def test_function_call_is_transparent(self):
+        """
+        With and without optimization the method
+        should be called with the same parameters.
+        """
+        error = BaseErrorMeasure()
+        error.local_error = Mock()
+        ts = TimeSeries.from_twodim_list([[1,1]])
 
-		error._enable_instance_optimization()
-		error.initialize(ts, ts)
+        error._enable_instance_optimization()
+        error.initialize(ts, ts)
 
-		error._disable_instance_optimization()
-		error.initialize(ts, ts)
+        error._disable_instance_optimization()
+        error.initialize(ts, ts)
 
-	def test_import_fail(self):
-		"""
-		If the import of the cmodules 
-		fails the original method should be used
-		"""
-		class Foo:
-			def __init__(self):
-				self.optimization_enabled = True
-				self.called = False
-			@optimized
-			def foo(self):
-				self.called = True
+    def test_import_fail(self):
+        """
+        If the import of the cmodules 
+        fails the original method should be used
+        """
+        class Foo:
+            def __init__(self):
+                self.optimization_enabled = True
+                self.called = False
+            @optimized
+            def foo(self):
+                self.called = True
 
-		test_obj = Foo()
-		test_obj.foo()
-		assert test_obj.called, "If no cmodule exists, original method should be called"
+        test_obj = Foo()
+        test_obj.foo()
+        assert test_obj.called, "If no cmodule exists, original method should be called"
