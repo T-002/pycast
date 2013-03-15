ea317d811c1578c7ff31661adf955eeadfc85a53
Christian Schwarz <christianschw4rz@gmail.com>
2013-03-15 20:52:58 +0100

    …..

--- pycast/common/timeseries.py
+++ pycast/common/timeseries.py
@@ -22,7 +22,7 @@
 #OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 #WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import time
+import time, random, os
 
 ## some string constants
 _STR_EPOCHS = "UNIX-epochs"
