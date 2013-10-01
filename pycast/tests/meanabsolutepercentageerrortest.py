83d2dbfd9a17e80a3ab0c7c0f8b140f1b3e34ac8
Alexander Franke <alexanderfranke1990@gmail.com>
2013-10-01 11:25:32 +0200

    MeanAbsolutePercentageErrorTest uses the new test data set now.

--- pycast/tests/meanabsolutepercentageerrortest.py
+++ pycast/tests/meanabsolutepercentageerrortest.py
@@ -32,26 +32,62 @@ import math
 
 class MeanAbsolutePercentageErrorTest(unittest.TestCase):
     """Test class containing all tests for MeanAbsolutePercentageError."""
+    
+    def setUp(self):
+        self.dataOrg = [1.0,    2.3,    0.1,    -2.0,   -1.0,   0.0,    -0.2,   -0.3,   0.15,   -0.2,   0]
+        self.dataCalc = [1.2,   2.0,    -0.3,   -1.5,   -1.5,   0.3,    0.0,    0.3,    -0.15,  0.3,   0]
+
+    def tearDown(self):
+        pass
+
     def local_error_test(self):
-        orgValues = [11, 33.1, 2.3, 6.54, 123.1, 12.54, 12.9]
-        calValues = [24, 1.23, 342, 1.21, 4.112, 9.543, 3.54]
+        """Test MeanAbsolutePercentageError.local_error."""
+        localErrors = [20,  13.043, 400,  25, 50,    None,  100,  200,  200,  250, None]
 
         mape = MeanAbsolutePercentageError()
-        for idx in xrange(len(orgValues)):
-            res = ((math.fabs(calValues[idx] - orgValues[idx]))/orgValues[idx])*100
-            assert  str(res)[:6] == str(mape.local_error([orgValues[idx]], [calValues[idx]]))[:6]
+
+        for i in xrange(len(self.dataOrg)):
+            calc_local_error = mape.local_error([self.dataOrg[i]], [self.dataCalc[i]])
+            if calc_local_error:
+                self.assertEquals("%.3f" % calc_local_error,"%.3f" % localErrors[i])
+            else:
+                self.assertEquals(localErrors[i], None)
 
     def error_calculation_test(self):
-        """Test the calculation of the MeanAbsolutePercentageError."""        
-        dataOrg         = [[1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,8], [7.3, 5], [8, 0], [9,10], [10, -10]]
-        dataCalc        = [[1,3], [2,5], [3,0], [4,3], [5,5], [6.1,6], [7,3], [7.3, 5], [8, 0], [9,9], [10, -15]]
-        # abs difference:     2      3      3      1      0       NA      5    0         NA       1         5
-        # local errors:       200    150    100    25     0       NA      62,5 0         NA       10        50
-        # sum: 597,5
-                
-        tsOrg  = TimeSeries.from_twodim_list(dataOrg)
-        tsCalc = TimeSeries.from_twodim_list(dataCalc)
+        """Test the calculation of the SymmetricMeanAbsolutePercentageError."""
+        tsOrg  = TimeSeries()
+        tsCalc = TimeSeries()
+        
+        for idx in xrange(len(self.dataOrg)):
+            tsOrg.add_entry(float(idx),  self.dataOrg[idx])
+            tsCalc.add_entry(float(idx), self.dataCalc[idx])
 
         mape = MeanAbsolutePercentageError()
         mape.initialize(tsOrg, tsCalc)
-        assert str(mape.get_error())[:6] == "66.388"
+
+        self.assertEquals("139.78", str(mape.get_error())[:6])
+
+
+#    def local_error_test(self):
+#        orgValues = [11, 33.1, 2.3, 6.54, 123.1, 12.54, 12.9]
+#        calValues = [24, 1.23, 342, 1.21, 4.112, 9.543, 3.54]
+#
+#        mape = MeanAbsolutePercentageError()
+#        for idx in xrange(len(orgValues)):
+#            res = ((math.fabs(calValues[idx] - orgValues[idx]))/orgValues[idx])*100
+#            assert  str(res)[:6] == str(mape.local_error([orgValues[idx]], [calValues[idx]]))[:6]
+#
+#    def error_calculation_test(self):
+#        """Test the calculation of the MeanAbsolutePercentageError."""        
+#        dataOrg         = [[1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,8], [7.3, 5], [8, 0], [9,10], [10, -10]]
+#        dataCalc        = [[1,3], [2,5], [3,0], [4,3], [5,5], [6.1,6], [7,3], [7.3, 5], [8, 0], [9,9], [10, -15]]
+#        # abs difference:     2      3      3      1      0       NA      5    0         NA       1         5
+#        # local errors:       200    150    100    25     0       NA      62,5 0         NA       10        50
+#        # sum: 597,5
+#                
+#        tsOrg  = TimeSeries.from_twodim_list(dataOrg)
+#        tsCalc = TimeSeries.from_twodim_list(dataCalc)
+#
+#        mape = MeanAbsolutePercentageError()
+#        mape.initialize(tsOrg, tsCalc)
+#        assert str(mape.get_error())[:6] == "66.388"
