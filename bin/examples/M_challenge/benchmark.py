4dbcd917b1fd4c01006798dfe56a3ae76ff25a3c
T-002 <christianschw4rz@gmail.com>
2015-08-10 14:48:14 +0200

    landscape.io changes for example

--- bin/examples/M_challenge/benchmark.py
+++ bin/examples/M_challenge/benchmark.py
@@ -1,3 +1,27 @@
+# !/usr/bin/env python
+#  -*- coding: UTF-8 -*-
+
+# Copyright (c) 2012-2015 Christian Schwarz
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import sys
 sys.path.append('../../../')
 
@@ -20,7 +44,7 @@ with open('MC1001_season_indices.csv','r') as season_indices:
 			for i in range(number_obeservations):
 				orig.add_entry(i, series_tuples[i + 7]) #offset to first data entry in line
 			#print orig
-			
+
 			forecast_check = TimeSeries(isNormalized=True)
 			for j in range(number_obeservations, number_obeservations + number_forecast):
 				forecast_check.add_entry(j, float(series_tuples[j + 7]))
